e9c65a8992a4653b850945dfa9419dba1223e830
Atul Agrawal <atulagrawal0711@gmail.com>
2023-11-14 18:59:22 +0100

    test commit point

---
+++ scoutNd/ackley_test.py
@@ -0,0 +1,221 @@
+
+from benchmarks import AckleyFunction
+import numpy as np
+import torch
+import pickle
+from matplotlib import pyplot as plt
+from matplotlib import rc
+from itertools import product
+
+from stochastic_optimizer import Stochastic_Optimizer
+from objective_function import Baseline1
+rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
+## for Palatino and other serif fonts use:
+#rc('font',**{'family':'serif','serif':['Palatino']})
+rc('text', usetex=True)
+# plt.style.use('ggplot')
+SMALL_SIZE = 8
+MEDIUM_SIZE = 12
+BIGGER_SIZE = 20
+
+rc('font', size=MEDIUM_SIZE)          # controls default text sizes
+rc('axes', titlesize=BIGGER_SIZE)     # fontsize of the axes title
+rc('axes', labelsize=BIGGER_SIZE)    # fontsize of the x and y labels
+rc('xtick', labelsize=BIGGER_SIZE)    # fontsize of the tick labels
+rc('ytick', labelsize=BIGGER_SIZE)    # fontsize of the tick labels
+rc('legend', fontsize=MEDIUM_SIZE)    # legend fontsize
+rc('figure', titlesize=MEDIUM_SIZE)  # fontsize of the figure titles
+
+# function = AckleyFunction(dim=2)
+# print(function.get_name())
+# function.evaluate_hf(np.array([0,0]))
+# fig = function.plot()
+
+dim = 2
+def function(x:np.array):
+    fn = AckleyFunction(dim=dim)
+    val1 = fn.evaluate_hf(x)
+    noise = False
+    if noise:
+        val2 = np.random.normal(0, 0.0001, val1.shape)
+        return val1+ val2
+    else:
+        return val1
+
+optimize= False
+gaussian_conv = False
+optimize_compare = False
+check_fim = True
+
+if optimize == True:
+    constraints = None
+    # constraints = None
+    obj = Baseline1(dim, function, constraints, num_samples=128, qmc=True, correct_constraint_derivative=True)
+    optimizer = Stochastic_Optimizer(obj)
+    x0 = np.ones(2*dim)
+    x0[dim:] = -10
+    #x0[dim:] = 0
+    #x0 = np.zeros(2*dim)
+    optimizer.set_initial_val(x0)
+    optimizer.create_optimizer('Adam', lr=1e-2)
+    optimizer.optimize(num_steps=300)
+    print(optimizer.get_final_state())
+
+if gaussian_conv:
+    sigma_set = [0,-10]
+    obj = Baseline1(dim, function, None, num_samples=64, qmc=True, correct_constraint_derivative=True)
+
+    # evaluate function in x in [-30,30] for 2 d
+    mesh_size = 100
+    x = np.linspace(-30, 30, mesh_size)
+    y = np.linspace(-30, 30, mesh_size)
+    X, Y = np.meshgrid(x, y)
+    temp = np.array(list(product(x,y)))
+
+    # plot without smootheing
+    fn = AckleyFunction(dim=dim)
+    zs = fn.evaluate_hf(temp)
+    Z = zs.reshape(mesh_size, mesh_size)
+    
+    # do a 3d contour plot
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot_surface(X, Y, Z, cmap='viridis')
+    ax.plot_wireframe(X, Y, Z, color='black', alpha=0.3, linewidth=0.3, rcount=mesh_size/5, ccount=mesh_size/5)
+    ax.set_xlabel(r'$x_1$')
+    ax.set_ylabel(r'$x_2$')
+    ax.set_zlabel(r'$f(x_1, x_2)$')
+    plt.tight_layout()
+    plt.savefig('ackley_no_conv.pdf')
+
+    #plt.show()
+
+
+    # x = np.zeros(2*dim)
+    # x[dim:] = sigma*np.ones(dim)
+    for sigma in sigma_set:
+        x = np.zeros(2*dim)
+        z_conv = np.zeros(mesh_size*mesh_size)
+        for i in range(mesh_size*mesh_size):
+            x[:dim] = temp[i]
+            x[dim:] = sigma*np.ones(dim)
+            fval, _ = obj.function_wrapper(torch.tensor(x, dtype=torch.float64))
+            z_conv[i] = fval
+        Z_s = z_conv.reshape(mesh_size, mesh_size)
+
+        # do a 3d contour plot
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.plot_surface(X, Y, Z_s, cmap='viridis')
+        ax.plot_wireframe(X, Y, Z_s, color='black', alpha=0.3, linewidth=0.3,rcount=mesh_size/5, ccount=mesh_size/5)
+        ax.set_xlabel(r'$\mu_1$')
+        ax.set_ylabel(r'$\mu_2$')
+        ax.set_zlabel(r'$U(\mu,\sigma)$')
+        plt.tight_layout()
+        plt.savefig(f'ackley_conv_' +f'sigma_{sigma}' +'.pdf')
+        #plt.show()
+    
+    
+if optimize_compare:
+    constraints = None
+    # constraints = None
+    obj = Baseline1(dim, function, constraints, num_samples=64, qmc=True, correct_constraint_derivative=True)
+    
+    x0 = 3*np.ones(2*dim)
+    sigma = -10
+    x0[dim:] = sigma
+    #x0[dim:] = sigma
+    #x0 = np.zeros(2*dim)
+    optimizer = Stochastic_Optimizer(obj, initial_val=x0)
+    #optimizer.set_initial_val(x0)
+    optimizer.create_optimizer('Adam', lr=1e-1)
+    num_steps = 1000
+    optimizer.optimize(num_steps=num_steps)
+    mean_evo = np.zeros((num_steps, dim))
+    var_evo = np.zeros((num_steps, dim))
+    # convert list to array
+    for i in range(num_steps):
+        mean_evo[i,:] = optimizer.stored_results[i].detach().numpy()[:dim]
+        var_evo[i,:] = np.exp(optimizer.stored_results[i].detach().numpy()[dim:])
+
+    # do a 2d contour plot for the Ackley fn
+    
+    # evaluate function in x in [-30,30] for 2 d
+    mesh_size = 100
+    x = np.linspace(-10, 10, mesh_size)
+    y = np.linspace(-10, 10, mesh_size)
+    X, Y = np.meshgrid(x, y)
+    temp = np.array(list(product(x,y)))
+
+    # plot without smootheing
+    fn = AckleyFunction(dim=dim)
+    zs = fn.evaluate_hf(temp)
+    Z = zs.reshape(mesh_size, mesh_size)
+
+    # 2d surfac plot
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    h = ax.contourf(X, Y, Z, cmap='viridis', levels=20)
+    # plot the evolution of the design variables
+    ax.plot(mean_evo[:,0], mean_evo[:,1], 'x', markersize=6, color='black')
+    # last marker red
+    ax.plot(mean_evo[-1,0], mean_evo[-1,1], 'x', markersize=8, color='red')
+    ax.set_xlabel(r'$x_1$')
+    ax.set_ylabel(r'$x_2$')
+    
+    # add colorbar
+    cbar = plt.colorbar(h, ax=ax)
+    plt.tight_layout()
+    plt.savefig('ackley_optimization_mean_evolution_contour_' + f'sigma_{sigma}'+'.pdf')
+    plt.show()
+
+    # plot mean evolution
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    iterations = np.linspace(0, num_steps, num_steps)
+    ax.plot(mean_evo[1:,0],'r', label=r'$\mu_1$')
+    ax.plot(mean_evo[1:,1],'b', label=r'$\mu_2$')
+    ax.set_xlabel(r'$iterations$')
+    ax.set_ylabel(r'$\mu$')
+    ax.legend()
+    ax.grid()
+    plt.tight_layout()  
+    plt.savefig('ackley_optimization_mean_evolution_' + f'sigma_{sigma}'+'.pdf')
+    plt.show()
+
+    # plot var evolution
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    iterations = np.linspace(0, num_steps, num_steps)
+    ax.semilogy(var_evo[1:,0],'r', label=r'$\sigma_1^2$')
+    ax.semilogy(var_evo[1:,1],'b', label=r'$\sigma_2^2$')
+    ax.set_xlabel(r'$iterations$')
+    ax.set_ylabel(r'$\sigma^2$')
+    ax.legend()
+    ax.grid()
+    plt.tight_layout()  
+    plt.savefig('ackley_optimization_var_evolution_' + f'sigma_{sigma}'+'.pdf')
+    plt.show()
+
+if check_fim:
+
+
+    
+
+
+
+        #print(optimizer.get_final_state())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ scoutNd/stochastic_optimizer.py
@@ -0,0 +1,214 @@
+import numpy as np
+import torch
+from objective_function import *
+import matplotlib.pyplot as plt
+from copy import deepcopy
+import sys
+seed = 0
+
+
+class Stochastic_Optimizer():
+    """Class that encapsulates all the optimization parameters.
+
+    """
+
+    def __init__(self, objective: ObjectiveAbstract, **kwargs:dict):
+        """Initializer
+        
+        Parameters
+        ----------
+        objective : ObjectiveAbstract
+            Object of type ObjectiveAbstract that contains all the information about the function and constraints.
+        """
+        self.objective = objective
+        self.dim = self.objective.dim
+        if 'initial_val' in kwargs.keys():
+            self.set_initial_val(kwargs['initial_val'])
+        else:
+            x0 = np.ones(2*self.dim)
+            x0[self.dim:] = -1
+            self.set_initial_val(x0)
+        self.stored_results = [self.initial_val]        
+        self.optimizer = None
+        if 'natural_gradients' in kwargs.keys():
+            self.natural_gradients = kwargs['natural_gradients']
+        else:
+            self.natural_gradients = False
+
+    def set_initial_val(self, initial_val:np.ndarray):
+        """_summary_
+
+        Parameters
+        ----------
+        initial_val : np.ndarray
+            Starting point of the optimization algorithm.
+        """
+        assert len(initial_val) == 2*self.dim, 'Incorrect length of initial values, it should be equal to 2*dim'
+        self.initial_val = torch.tensor(initial_val, requires_grad=True)
+        self.parameters = [self.initial_val] # TODO: transfer it to objective. This does not make sense in optimizer
+
+    def create_optimizer(self, name_optimizer:str, **optimizer_parameters:dict):
+        name_optimizer = name_optimizer.lower()
+        if name_optimizer == 'adam':
+            self.optimizer = torch.optim.Adam(self.parameters, **optimizer_parameters)
+        elif name_optimizer == 'adadelta':
+            self.optimizer = torch.optim.Adadelta(self.parameters, **optimizer_parameters)
+        elif name_optimizer == 'adagrad':
+            self.optimizer = torch.optim.Adagrad(self.parameters, **optimizer_parameters)
+        elif name_optimizer == 'adamw':
+            self.optimizer = torch.optim.AdamW(self.parameters, **optimizer_parameters)
+        elif name_optimizer == 'adamax':
+            self.optimizer = torch.optim.Adamax(self.parameters, **optimizer_parameters)
+        elif name_optimizer == 'asgd':
+            self.optimizer = torch.optim.ASGD(self.parameters, **optimizer_parameters)
+        elif name_optimizer == 'nadam':
+            self.optimizer = torch.optim.NAdam(self.parameters, **optimizer_parameters)
+        elif name_optimizer == 'radam':
+            self.optimizer = torch.optim.RAdam(self.parameters, **optimizer_parameters)
+        elif name_optimizer == 'rmsprop':
+            self.optimizer = torch.optim.RMSprop(self.parameters, **optimizer_parameters)
+        elif name_optimizer == 'rprop':
+            self.optimizer = torch.optim.Rprop(self.parameters, **optimizer_parameters)
+        elif name_optimizer == 'SGD':
+            self.optimizer = torch.optim.SGD(self.parameters, **optimizer_parameters)
+        else:
+            raise NotImplementedError('Either the name of optimizer is wrong or it is not yet implemented in the code')
+
+    def optimize_fixed_lambda(self, log_lambdas:np.ndarray, num_steps_per_lambda:int):
+        """Performs optimization for fix value of lambdas for the given number of steps.
+
+        Parameters
+        ----------
+        log_lambdas : np.ndarray
+            The exponential logarithm of the penalty scaling factor.
+        num_steps_per_lambda : int
+            Number of optimization steps with fixed value of lambdas.
+        """
+        self.objective.update_lambdas(log_lambdas)
+        for i in range(num_steps_per_lambda):
+            val, grad = self.objective.function_wrapper(self.parameters[0])
+            if self.natural_gradients:
+                grad = self._get_fim_adjusted_gradient(self.parameters[0][self.dim:].detach().numpy(), grad)
+            self.parameters[0].grad = torch.tensor(grad) # Tis could be problemtic in GPUs when device is not set correctly
+            self.optimizer.step()
+            self.stored_results.append(deepcopy(self.parameters[0]))
+    
+    def _get_fim_adjusted_gradient(self, phi:np.ndarray, grad:np.ndarray):
+        """Computes the adjusted gradient using the Fisher information matrix.
+
+        Parameters
+        ----------
+        phi : np.ndarray
+            sigma**2  = e^(2*phi)
+        grad : np.ndarray [2d,1]
+            Gradient of the objective function.
+
+        Returns
+        -------
+        np.ndarray
+            Adjusted gradient.
+        """
+        assert phi.shape == (self.dim,), 'Incorrect shape of phi'
+        assert grad.shape == (2*self.dim,), 'Incorrect shape of grad'
+        fim = self._fisher_information_matrix(phi)
+        #TODO add damped fim here
+        tilda_grad_U = np.linalg.solve(fim, grad)
+        return tilda_grad_U
+    
+    def _fisher_information_matrix(self,phi):
+        """Computes the Fisher information matrix.
+
+        Parameters
+        ----------
+        phi : np.ndarray 
+            sigma**2  = e^(2*phi)
+
+        Returns
+        -------
+        np.ndarray
+            Fisher information matrix.
+        """
+        assert phi.shape == (self.dim,), 'Incorrect shape of phi'
+        tmp = np.exp(-2*phi)
+        fisher_diag = np.hstack((tmp,2*np.ones(self.dim)))
+        fim = np.diag(fisher_diag)
+
+        # check if FIM is invertible
+        if np.linalg.cond(fim) > 1/sys.float_info.epsilon:
+            raise ValueError('Fisher information matrix is not invertible')
+        return np.diag(fisher_diag)
+
+    def constrained_optimization(self, initial_log_lambdas:int=-1, num_lambdas:int=4, num_steps_per_lambda:int=100):
+        """Function that performs constrained optimization.
+
+        Parameters
+        ----------
+        initial_log_lambdas : int, optional
+            starting values of exponential logarithm of lambda, by default -1
+        num_lambdas : int, optional
+            Number of lambda updates, by default 4
+        num_steps_per_lambda : int, optional
+            Number of optimization steps with fixed value of lambdas., by default 100
+        """
+        if self.optimizer is None:
+            print('Optimizer is not created, reverting to default Adam optimizer with lr 1e-2')
+            self.optimizer = torch.optim.Adam(self.parameters[0], lr=1e-2)
+        lambdas = initial_log_lambdas*np.ones(self.objective.num_constraints) 
+        for i in range(num_lambdas):
+            self.optimize_fixed_lambda(lambdas, num_steps_per_lambda)
+            print(self.objective.lambdas, self.get_final_state())
+            lambdas = lambdas + 1
+
+    def unconstrained_optimization(self, num_steps: int=100):
+        """Function that performs unconstrained optimization.
+
+        Parameters
+        ----------
+        num_steps : int, optional
+            number of optimization steps, by default 100
+        """
+        for i in range(num_steps):
+            val, grad = self.objective.function_wrapper(self.parameters[0])
+            self.parameters[0].grad = torch.tensor(grad) # Tis could be problemtic in GPUs when device is not set correctly
+            self.optimizer.step()
+            self.stored_results.append(deepcopy(self.parameters[0]))
+    
+    def optimize(self, **kwargs):
+        """Function to optmize the objective function
+        """
+        if self.objective.num_constraints > 0:
+            self.constrained_optimization(**kwargs)
+        else:
+            self.unconstrained_optimization(**kwargs)
+    
+    def get_final_state(self):
+        """Reurns the finals optimization results.
+
+        Returns
+        -------
+        dict
+            Dictionary of optimum mean and variance.
+        """
+        return {'mean':self.stored_results[-1][:self.dim], 'variance':self.stored_results[-1][self.dim:]}
+    
+
+def sphere(x):
+    X = np.atleast_2d(x)
+    val1 = np.sum(X**2, axis=1)
+    # val2 = np.random.normal(0, 0.0001, val1.shape)
+    val2 = 0
+    return val1 + val2
+
+def linear_constraint(X):
+    x = np.atleast_2d(X)
+    return 1 - x[:, 0] - x[:, 1]
+
+if __name__ == '__main__':
+    dim = 32
+    constraints = [linear_constraint]
+    # constraints = None
+    obj = Baseline1(dim, sphere, constraints, num_samples=16, qmc=True, correct_constraint_derivative=True)
+    optimizer = Stochastic_Optimizer(obj)
+    optimizer.create_optimizer('Adam', lr=1e-2)
+    optimizer.optimize()
+    print(optimizer.get_final_state())
